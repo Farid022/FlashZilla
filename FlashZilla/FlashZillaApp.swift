31d156537e39592d0a41f0239cac143f0dc277a9
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2023-01-24 14:21:22 +0500

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Muhammad Farid Ullah on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
